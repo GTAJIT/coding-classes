001eb56ea8eda35ee88702923269f4416af2055c
Jit Mukherjee <133637574+GTAJIT@users.noreply.github.com>
2025-01-02 14:05:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-a = "shreya"
-print(len(a))
-print(a[0:6:1])
+# a = "shreya" #012345 = 6
+# b = len(a)
+# # c = a[1:5:2]
+# c = a[:-1]
+# print(c)
+# a = input("Your name: ")
+# print(f"Hi, its {a}\ndate: 2/1/25\tyo!! \\shhhh \'hi\'")
